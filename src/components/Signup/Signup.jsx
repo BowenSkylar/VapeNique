729afe353cb5e9b1a739cdc50e4524b22a192271
Skylar Bowen <bowenskylar>
2016-12-18 19:44:56 -0500

    trying sign in again

--- src/components/Signup/Signup.jsx
+++ src/components/Signup/Signup.jsx
@@ -1,18 +1,18 @@
 import React, { Component } from 'react';
 import style from './Signup.css';
 
-// <input onChange={this.props.trackSignupForm} type="text" placeholder="Desired Username"/>
-//         <input onChange={this.props.trackSignupForm} type="text" placeholder="Desired Password"/>
-//         <button className="signupBtn" onClick={this.props.postSignup}>Sign Up</button>
 
+        // <h5>Instructions: <br/></h5>
+        // <p>Please Log in with <br/>
+        // Username: yo & Password: yo</p>
 
 class Signup extends Component {
   render() {
     return(
       <div>
-        <h5>Instructions: <br/></h5>
-        <p>Please Log in with <br/>
-        Username: yo & Password: yo</p>
+// <input onChange={this.props.trackSignupForm} type="text" placeholder="Desired Username"/>
+//         <input onChange={this.props.trackSignupForm} type="text" placeholder="Desired Password"/>
+//         <button className="signupBtn" onClick={this.props.postSignup}>Sign Up</button>
       </div>
     )
   }
