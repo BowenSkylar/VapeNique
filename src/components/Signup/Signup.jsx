2950833d5e883d265ddffc9ee3ac56ba238cf257
Skylar Bowen <bowenskylar>
2016-12-18 19:48:48 -0500

    .

--- src/components/Signup/Signup.jsx
+++ src/components/Signup/Signup.jsx
@@ -10,9 +10,9 @@ class Signup extends Component {
   render() {
     return(
       <div>
-// <input onChange={this.props.trackSignupForm} type="text" placeholder="Desired Username"/>
-//         <input onChange={this.props.trackSignupForm} type="text" placeholder="Desired Password"/>
-//         <button className="signupBtn" onClick={this.props.postSignup}>Sign Up</button>
+<input onChange={this.props.trackSignupForm} type="text" placeholder="Desired Username"/>
+         <input onChange={this.props.trackSignupForm} type="text" placeholder="Desired Password"/>
+         <button className="signupBtn" onClick={this.props.postSignup}>Sign Up</button>
       </div>
     )
   }
