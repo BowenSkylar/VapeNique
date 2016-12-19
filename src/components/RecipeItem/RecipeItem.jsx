911cd9202349824a79a2e391d2a74e2522717f25
Skylar Bowen <bowenskylar>
2016-12-18 19:55:32 -0500

    USER SIGN UP IS WORKING

--- src/components/RecipeItem/RecipeItem.jsx
+++ src/components/RecipeItem/RecipeItem.jsx
@@ -3,7 +3,7 @@ import style from './RecipeItem.css';
 
 const RecipeItem = props => (
   <div className="recipeItem">
-    <img className="trashcan" src="http://findicons.com/files/icons/1580/devine_icons_part_2/128/trash_recyclebin_empty_closed.png"/>
+    <img className="trashcan" onClick={deleteRecipe} src="http://findicons.com/files/icons/1580/devine_icons_part_2/128/trash_recyclebin_empty_closed.png"/>
     <p>Recipe Name: {props.recipe_name}</p>
     <p>Nicotine Level: {props.nicotine}</p>
     <p>Size: {props.size}</p>
